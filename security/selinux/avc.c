cde738fb2a848deac5bce3c13335c024301ec253
Soul Spark <spark_soul@mail.ru>
2026-04-18 08:33:33 +0000

    chore: disable dirty version tagging and fix variable reference in SELinux avc log spoofing

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -182,10 +182,6 @@ static void avc_dump_query(struct audit_buffer *ab, struct selinux_state *state,
 	int rc;
 	char *scontext;
 	u32 scontext_len;
-#ifdef CONFIG_KSU_SUSFS
-	struct selinux_audit_data sad;
-#endif
-
 	rc = security_sid_to_context(state, ssid, &scontext, &scontext_len);
 
 	if (rc)
@@ -199,7 +195,7 @@ static void avc_dump_query(struct audit_buffer *ab, struct selinux_state *state,
 
 
 #ifdef CONFIG_KSU_SUSFS
-	if (unlikely(sad.tsid == susfs_ksu_sid && READ_ONCE(susfs_is_avc_log_spoofing_enabled))) {
+	if (unlikely(tsid == susfs_ksu_sid && READ_ONCE(susfs_is_avc_log_spoofing_enabled))) {
 		if (rc)
 			audit_log_format(ab, " tsid=%d", susfs_priv_app_sid);
 		else
